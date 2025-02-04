93f1b7f7614d5e104259b0b677417ea72e4c3f50
andre <andreas.m4063@gmail.com>
2025-02-04 12:37:29 +0100

    -- updated layout of measurement-two-frame to include customer logo as tab

--- src/app/views/measurement-two-frame/measurement-two-frame.component.ts
+++ src/app/views/measurement-two-frame/measurement-two-frame.component.ts
@@ -17,7 +17,7 @@ import {firstValueFrom} from 'rxjs';
   styleUrl: './measurement-two-frame.component.css'
 })
 export class MeasurementTwoFrameComponent {
-  tab_label : string = 'CREFIX APP';
+  tab_label : string = '';
   back_button_label: string = 'Zurück';
   forward_button_label: string = 'Weiter';
   floor_label: string = 'Stockwerk / Wohnung / Raum';
