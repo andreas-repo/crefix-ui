310e97f37ab63dd2f791bbff0f84386868e07635
andre <andreas.m4063@gmail.com>
2025-02-04 12:38:55 +0100

    -- updated layout of measurement-three-frame to include customer logo as tab

--- src/app/views/measurement-three-frame/measurement-three-frame.component.ts
+++ src/app/views/measurement-three-frame/measurement-three-frame.component.ts
@@ -17,7 +17,7 @@ import {firstValueFrom} from 'rxjs';
   styleUrl: './measurement-three-frame.component.css'
 })
 export class MeasurementThreeFrameComponent {
-  tab_label : string = 'CREFIX APP';
+  tab_label : string = '';
   back_button_label: string = 'Zurück';
   forward_button_label: string = 'Weiter';
   floor_label: string = 'Stockwerk / Wohnung / Raum';
