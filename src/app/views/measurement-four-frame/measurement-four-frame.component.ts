a081be22e12c84382355d70a8c1ba76120455e72
andre <andreas.m4063@gmail.com>
2025-02-04 12:40:52 +0100

    -- updated layout of measurement-four-frame to include customer logo as tab

--- src/app/views/measurement-four-frame/measurement-four-frame.component.ts
+++ src/app/views/measurement-four-frame/measurement-four-frame.component.ts
@@ -17,7 +17,7 @@ import {firstValueFrom} from 'rxjs';
   styleUrl: './measurement-four-frame.component.css'
 })
 export class MeasurementFourFrameComponent {
-  tab_label : string = 'CREFIX APP';
+  tab_label : string = 'Vierter Messpunkt';
   back_button_label: string = 'Zurück';
   forward_button_label: string = 'Weiter';
   floor_label: string = 'Stockwerk / Wohnung / Raum';
